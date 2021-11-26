e4275100b638cc54d53c4a0c7895cc469ac3405b
anveshdamulur <anveshdamuluri1226@gmail.com>
2021-11-26 22:31:01 +0200

    initial commit: npx create-nx-workspace --preset=react

---
+++ apps/red-acre-tech-assignment/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'red-acre-tech-assignment',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/red-acre-tech-assignment',
+};
