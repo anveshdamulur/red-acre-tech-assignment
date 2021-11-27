443488d4c949420f134384a559a145b12a0bdb07
anveshdamulur <anveshdamuluri1226@gmail.com>
2021-11-27 09:16:05 +0200

    #3: add sample data and display in mapbox-map

--- apps/red-acre-tech-assignment/src/app/mapbox-map/mapbox-map.tsx
+++ apps/red-acre-tech-assignment/src/app/mapbox-map/mapbox-map.tsx
@@ -19,7 +19,7 @@ mapboxgl.accessToken =
   'pk.eyJ1IjoiYW52ZXNoZGFtdWx1ciIsImEiOiJja3dndW9oYmQwc2M3MnBwbTk3aHk2ODhvIn0.cST7lXwv2m_th18JJnMq4g';
 
 const MapboxMap = (props: MapboxMapProps) => {
-  const mapContainer = useRef(null);
+  const mapContainerRef = useRef(null);
 
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
@@ -29,14 +29,34 @@ const MapboxMap = (props: MapboxMapProps) => {
     // if (map.current) return; // initialize map only once
 
     const map = new mapboxgl.Map({
-      container: mapContainer.current || '',
-      style: 'mapbox://styles/mapbox/streets-v11',
+      container: mapContainerRef.current || '',
+      style: 'mapbox://styles/mapbox/light-v10',
       center: [lng, lat],
       zoom: zoom,
     });
 
     // add navigation control (the +/- zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    map.on('load', () => {
+      map.addSource('popeye-village-balluta', {
+        type: 'geojson',
+        // Use a URL for the value for the `data` property.
+        data: '/assets/geojson/popeye-village-balluta.geojson',
+      });
+
+      map.addLayer({
+        id: 'popeye-village-balluta-layer',
+        type: 'circle',
+        source: 'popeye-village-balluta',
+        paint: {
+          'circle-radius': 8,
+          'circle-stroke-width': 2,
+          'circle-color': 'red',
+          'circle-stroke-color': 'white',
+        },
+      });
+    });
 
     // clean up on unmount
     return () => map.remove();
@@ -45,7 +65,7 @@ const MapboxMap = (props: MapboxMapProps) => {
   return (
     <StyledMapboxMap>
       <div>
-        <div ref={mapContainer} className="map-container" />
+        <div ref={mapContainerRef} className="map-container" />
       </div>
     </StyledMapboxMap>
   );
